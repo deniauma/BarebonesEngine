17e9abb33a569d4cad15475cc08c4bb5fb44dbab
Maryvonne Lemeunier <you@example.com>
2018-12-22 19:12:57 +0100

    Generate a OpenGL context

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
-#include <windows.h>
+#include <glad/glad.h>
+#include <iostream>
 
+#pragma comment (lib, "opengl32.lib")
 
 void OnSize(HWND hwnd, UINT flag, int width, int height);
 
@@ -8,33 +10,20 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	switch (uMsg)
 	{
 	case WM_SIZE:
-	{
-		int width = LOWORD(lParam);  // Macro to get the low-order word.
-		int height = HIWORD(lParam); // Macro to get the high-order word.
+		{
+			int width = LOWORD(lParam);  // Macro to get the low-order word.
+			int height = HIWORD(lParam); // Macro to get the high-order word.
 
-									 // Respond to the message:
-		OnSize(hwnd, (UINT)wParam, width, height);
-	}
-	case WM_PAINT:
-	{
-		PAINTSTRUCT ps;
-		HDC hdc = BeginPaint(hwnd, &ps);
-
-
-
-		// All painting occurs here, between BeginPaint and EndPaint.
-
-		FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-
-		EndPaint(hwnd, &ps);
-	}
-	return 0;
+										 // Respond to the message:
+			OnSize(hwnd, (UINT)wParam, width, height);
+		}
+		return 0;
 	case WM_CLOSE:
 		DestroyWindow(hwnd);
-		break;
+		return 0;
 	case WM_DESTROY:
 		PostQuitMessage(0);
-		break;
+		return 0;
 	default:
 		return DefWindowProc(hwnd, uMsg, wParam, lParam);
 	}
@@ -51,12 +40,15 @@ int createWindow(int height, int width)
 {
 	// Register the window class.
 	const char CLASS_NAME[] = "Sample Window Class";
+	bool bQuit = false;
 
 	WNDCLASS wc = {};
 
 	wc.lpfnWndProc = WindowProc;
 	wc.hInstance = GetModuleHandle(NULL);
 	wc.lpszClassName = CLASS_NAME;
+	wc.hbrBackground = (HBRUSH)(COLOR_BACKGROUND);
+	wc.style = CS_OWNDC;
 
 	RegisterClass(&wc);
 
@@ -68,7 +60,7 @@ int createWindow(int height, int width)
 		WS_OVERLAPPEDWINDOW,            // Window style
 
 										// Size and position
-		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+		0, 0, 800, 600,
 
 		NULL,       // Parent window    
 		NULL,       // Menu
@@ -78,70 +70,85 @@ int createWindow(int height, int width)
 
 	if (hwnd == NULL)
 	{
-		return 0;
+		return -1;
 	}
 
+	PIXELFORMATDESCRIPTOR pfd =
+	{
+		sizeof(PIXELFORMATDESCRIPTOR),
+		1,
+		PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,    //Flags
+		PFD_TYPE_RGBA,        // The kind of framebuffer. RGBA or palette.
+		32,                   // Colordepth of the framebuffer.
+		0, 0, 0, 0, 0, 0,
+		0,
+		0,
+		0,
+		0, 0, 0, 0,
+		24,                   // Number of bits for the depthbuffer
+		8,                    // Number of bits for the stencilbuffer
+		0,                    // Number of Aux buffers in the framebuffer.
+		PFD_MAIN_PLANE,
+		0,
+		0, 0, 0
+	};
+
+	HDC ourWindowHandleToDeviceContext = GetDC(hwnd);
+
+	int letWindowsChooseThisPixelFormat;
+	letWindowsChooseThisPixelFormat = ChoosePixelFormat(ourWindowHandleToDeviceContext, &pfd);
+	SetPixelFormat(ourWindowHandleToDeviceContext, letWindowsChooseThisPixelFormat, &pfd);
+
+	HGLRC ourOpenGLRenderingContext = wglCreateContext(ourWindowHandleToDeviceContext);
+	wglMakeCurrent(ourWindowHandleToDeviceContext, ourOpenGLRenderingContext);
+
 	ShowWindow(hwnd, SW_SHOWDEFAULT);
-	UpdateWindow(hwnd);
+
+	//if (!gladLoadGLLoader((GLADloadproc)wglGetProcAddress))
+	if (!gladLoadGL())
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
 
 	MSG msg = {};
-	while (GetMessage(&msg, NULL, 0, 0))
+	/*while (GetMessage(&msg, NULL, 0, 0))
 	{
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
+	}*/
+
+	while (!bQuit) {
+		while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+			if (msg.message == WM_QUIT)
+			{
+				bQuit = TRUE;
+			}
+			else
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+			
+		}
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		SwapBuffers(ourWindowHandleToDeviceContext);
 	}
+
+	wglMakeCurrent(NULL, NULL);
+	wglDeleteContext(ourOpenGLRenderingContext);
+	ReleaseDC(hwnd, ourWindowHandleToDeviceContext);
+	DestroyWindow(hwnd);
+
+	return 0;
 }
 
 int main()
 {
-	createWindow(640, 480);
+	int res = createWindow(640, 480);
 
-	return 0;
+	return res;
 }
-
-/*int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR pCmdLine, int nCmdShow)
-{
-	// Register the window class.
-	const char CLASS_NAME[] = "Sample Window Class";
-
-	WNDCLASS wc = {};
-
-	wc.lpfnWndProc = WindowProc;
-	wc.hInstance = hInstance;
-	wc.lpszClassName = CLASS_NAME;
-
-	RegisterClass(&wc);
-
-	// Create the window.
-
-	HWND hwnd = CreateWindowEx(
-		0,                              // Optional window styles.
-		CLASS_NAME,                     // Window class
-		"Learn to Program Windows",    // Window text
-		WS_OVERLAPPEDWINDOW,            // Window style
-
-										// Size and position
-		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-
-		NULL,       // Parent window    
-		NULL,       // Menu
-		hInstance,  // Instance handle
-		NULL        // Additional application data
-	);
-
-	if (hwnd == NULL)
-	{
-		return 0;
-	}
-
-	ShowWindow(hwnd, nCmdShow);
-
-	MSG msg = {};
-	while (GetMessage(&msg, NULL, 0, 0))
-	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
-
-	return 0;
-}*/
